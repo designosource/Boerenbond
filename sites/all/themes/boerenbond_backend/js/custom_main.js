3ecf951b3a1bb7fe14d166da39303ed8b7f4013a
deroywa <calibrate@calibrates-MacBook-Pro.local>
2014-10-22 00:37:18 +0200

    core update + got rid of useless .txt files + /tmp folder bug fix

--- sites/all/themes/boerenbond_backend/js/custom_main.js
+++ sites/all/themes/boerenbond_backend/js/custom_main.js
@@ -1,10 +1,10 @@
 ;(function($) {
 	// color values
-	var green = "#aaffaa";
-	var blue = "#2fa5e8";
-	var red = "#faafbe";
-	var yellow = "#edee78";
-	var purple = "#c678ee";
+	var green 	= "#aaffaa";
+	var blue 	= "#2fa5e8";
+	var red 	= "#faafbe";
+	var yellow 	= "#edee78";
+	var purple 	= "#c678ee";
 	// on page load
 	$(document).ready(function() {
 		setColors();
@@ -15,10 +15,11 @@
 	});
 	// function that gets the parent of class (table row) and sets style
 	function setColors() {
-		$(".Gepubliceerd").parents('tr').css('background', green);
-		$(".redactieverantwoordelijke").parents('tr').css('background', blue);
-		$(".Voorstel").parents('tr').css('background', red);
-		$(".dossierbeheerder").parents('tr').css('background', purple);
-		$(".hoofdredacteur").parents('tr').css('background', yellow);
+		$(".Gepubliceerd")				.parents('tr')	.css('background', green);
+		$(".redactieverantwoordelijke")	.parents('tr')	.css('background', blue);
+		$(".Voorstel")					.parents('tr')	.css('background', red);
+		$(".dossierbeheerder")			.parents('tr')	.css('background', purple);
+		$(".hoofdredacteur")			.parents('tr')	.css('background', yellow);
+		//$(".redacteur")					.parents('tr')	.css('background', yellow);
 	}
 })(jQuery);
